25c6b6637103ee26c23e49673c462d94cbfc0bba
bsordyl <sordylbartlomiej@gmail.com>
2024-01-06 15:32:00 +0100

    added some features

---
+++ data.js
@@ -0,0 +1,113 @@
+export const tweetsData = [   
+    {
+        handle: `@Woody 🤠`,
+        profilePic: `images/woody.jpg`,
+        likes: 1822,
+        retweets: 54,
+        tweetText: `Howdy partners! Just rounding up some Toy Story fun in Andy's room. #ToyStoryAdventure 🤠🚀`,
+        replies: [
+            {
+                handle: `@BuzzLightyear 🚀`,
+                profilePic: `images/buzz.jpg`,
+                tweetText: `To infinity and beyond, Woody! Count me in for the next space mission. 🌌👨‍🚀`,
+            },
+            {
+                handle: `@BoPeep 🐑`,
+                profilePic: `images/bopeep.jpg`,
+                tweetText: `Woody, you always know how to keep the toys together. You're our sheriff! 🤠💙`,
+            },
+            {
+                handle: `@SlinkyDog 🐶`,
+                profilePic: `images/slinkydog.jpg`,
+                tweetText: `Woody, my pal! How about a game of fetch later? I'll bring the squeaky toys. 🎾🐾`,
+            },
+
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '1a9g5sdf-c0f5-4545-9c4b-88a56jg1l3',
+    },
+    {
+        handle: `@MickeyMouse 🐭`,
+        profilePic: `images/mickey.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Oh boy! Just finished a magical day at the park. #DisneyMagic ✨🏰`,
+        replies: [
+            {
+                handle: `@DonaldDuck 🦆`,
+                profilePic: `images/donald.jpg`,
+                tweetText: `Quacktastic day indeed! Let's do it again soon. #FunTimes 🎉`,
+            },
+            {
+                handle: `@Goofy 🐾`,
+                profilePic: `images/goofy.jpg`,
+                tweetText: `Gawrsh, missed the fun! Count me in for the next adventure. #GoofyAdventures 🚀`,
+            },
+            {
+                handle: `@DaisyDuck 🌼`,
+                profilePic: `images/daisy.jpg`,
+                tweetText: `Mickey, dear! Let's have a spa day at Minnie's Bow-tique. Pampering time for all of us! 💆‍♀️🌸`,
+            },
+
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '5a2f8sdf-c0f5-6jg8-9c4b-88a56jg1l3',
+    },
+   
+    {
+        handle: `@Elsa ❄️`,
+        profilePic: `images/elsa.jpg`,
+        likes: 55,
+        retweets: 12,
+        tweetText: `Let it go, let it go! ❄️✨ Embracing the winter magic. #FrozenMagic`,
+        replies: [
+            {
+                handle: `@Anna 👑`,
+                profilePic: `images/anna.jpg`,
+                tweetText: `You're making Arendelle sparkle, Elsa! ⛄️❤️`,
+            },
+            {
+                handle: `@Olaf ⛄️`,
+                profilePic: `images/olaf.jpg`,
+                tweetText: `I'm melting with joy just reading this! ⛄️😄`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '7b1c23s5-c0f5-4545-9c4b-88a56jg1l3',
+    },
+    {
+        handle: `@Simba 🦁`,
+        profilePic: `images/simba.jpg`,
+        likes: 15223,
+        retweets: 4110,
+        tweetText: `Hakuna Matata! Just took a stroll on Pride Rock. #LionKing 🌅👑`,
+        replies: [
+            {
+                handle: `@TimonAndPumbaa 🐭🐗`,
+                profilePic: `images/timonpumba.jpg`,
+                tweetText: `No worries, Simba! The jungle is always a good place for adventures. #HakunaMatata 🌿`,
+            },
+            {
+                handle: `@Nala 🦁❤️`,
+                profilePic: `images/nala.jpg`,
+                tweetText: `I love Pride Rock sunsets! Let's plan a dinner date under the stars. 🌟🍽️`,
+            },
+            {
+                handle: `@Rafiki 🐒`,
+                profilePic: `images/rafiki.jpg`,
+                tweetText: `Simba, my friend! Remember, the past can hurt. But the way I see it, you can either run from it or learn from it. 🙏🦁`,
+              },
+              {
+                handle: `@Zazu 🦜`,
+                profilePic: `images/zazu.jpg`,
+                tweetText: `Simba, the king of the Pride Lands! Your majesty, any royal decrees for today? 🤴👑`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '6g9r21ds-c0f5-8jh5-9c4b-88a56jg1l3',
+    }
+];
---
+++ index.js
@@ -0,0 +1,231 @@
+import { tweetsData as initialTweetsData} from './data.js'
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
+
+let tweetsData = []
+
+const storedTweets = JSON.parse(localStorage.getItem('tweets'))
+if(storedTweets){
+    tweetsData = storedTweets
+}else{
+    tweetsData = initialTweetsData
+}
+
+document.addEventListener('click', function(e){
+    if(e.target.id === 'h1'){
+        refreshPage()
+    }
+    else if(e.target.dataset.like){
+       handleLikeClick(e.target.dataset.like) 
+    }
+    else if(e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
+    }
+    else if(e.target.dataset.reply){
+        handleReplyClick(e.target.dataset.reply)
+    }
+    else if(e.target.id === 'tweet-btn'){
+        handleTweetBtnClick()
+    }
+    else if(e.target.dataset.tweetreply){
+        replyBtnClick(e.target.dataset.tweetreply)
+    }
+    else if(e.target.dataset.delete){
+        handleTrashClick(e.target.dataset.delete)
+    }
+})
+
+function refreshPage(){
+    window.location.reload()
+}
+
+function handleLikeClick(tweetId){ 
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+   
+    if (targetTweetObj.isLiked){
+        targetTweetObj.likes--
+    }
+    else{
+        targetTweetObj.likes++ 
+    }
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    setLocalStorage()
+    render()
+}
+
+
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+    
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    setLocalStorage()
+    render()
+}
+
+function handleReplyClick(replyId){
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+
+}
+
+function handleTweetBtnClick(){
+    const tweetInput = document.getElementById('tweet-input')
+
+    if(tweetInput.value){
+        tweetsData.unshift({
+            handle: `@WaltDisney`,
+            profilePic: `images/waltdisney.png`,
+            likes: 0,
+            retweets: 0,
+            tweetText: tweetInput.value,
+            replies: [],
+            isLiked: false,
+            isRetweeted: false,
+            isNew: true,
+            uuid: uuidv4()
+        })
+        setLocalStorage()
+        render()
+        tweetInput.value = ''
+    }
+}
+
+ function replyBtnClick(tweetId){
+    const replyInput = document.getElementById(`reply-input-${tweetId}`)
+     const relatedTweet = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+     })[0]    
+     
+       if(replyInput.value){
+            relatedTweet.replies.unshift({
+            handle: `@WaltDisney`,
+            profilePic: `images/waltdisney.png`,
+            tweetText: replyInput.value,
+            uuid:uuidv4()
+            })
+    }
+        setLocalStorage()
+        render()
+        document.getElementById(`replies-${tweetId}`).classList.remove('hidden')
+}
+
+function handleTrashClick(tweetId){
+    const targetTweetObj = tweetsData.findIndex(function(tweet){
+        return tweet.uuid === tweetId
+    })
+
+    tweetsData.splice(targetTweetObj, 1)
+    setLocalStorage()
+    render()
+}
+
+function setLocalStorage(){
+    localStorage.setItem('tweets', JSON.stringify(tweetsData))
+}
+
+function getFeedHtml(){
+    let feedHtml = ``
+
+    tweetsData.forEach(function(tweet){
+     
+        let likeIconClass = ''
+        
+        if (tweet.isLiked){
+            likeIconClass = 'liked'
+        }
+        
+        let retweetIconClass = ''
+        
+        if (tweet.isRetweeted){
+            retweetIconClass = 'retweeted'
+        }
+        
+        let repliesHtml = ''
+        
+        if(tweet.replies.length > 0){
+            tweet.replies.forEach(function(reply){
+                repliesHtml+=`
+                <div class="tweet-reply">
+                    <div class="tweet-inner">
+                        <img src="${reply.profilePic}" class="profile-pic">
+                            <div>
+                                <p class="handle">${reply.handle}</p>
+                                <p class="tweet-text">${reply.tweetText}</p>
+                            </div>
+                        </div>
+                </div>
+                `
+            })
+        }
+
+        let deleteButton = ''
+
+          if(tweet.isNew){
+            deleteButton = 
+            `<span class="tweet-detail">
+                <i class="fa-solid fa-trash"
+                data-delete="${tweet.uuid}"
+                ></i>
+            </span>`
+          }
+
+
+        feedHtml += `
+        <div class="tweet">
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details" id="tweet-details-${tweet.uuid}">
+                        <span class="tweet-detail">
+                            <i class="fa-regular fa-comment-dots"
+                            data-reply="${tweet.uuid}"
+                            ></i>
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-heart ${likeIconClass}"
+                            data-like="${tweet.uuid}"
+                            ></i>
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-retweet ${retweetIconClass}"
+                            data-retweet="${tweet.uuid}"
+                            ></i>
+                            ${tweet.retweets}
+                        </span>
+                        ${deleteButton}
+                    </div>   
+                </div>            
+            </div>
+            <div class="hidden" id="replies-${tweet.uuid}">
+                <img 
+                    src="images/waltdisney.png" 
+                    class="profile-pic">
+                <textarea placeholder="Post your reply" id="reply-input-${tweet.uuid}" class="reply-input" ></textarea>
+                <button id="reply-btn" class="reply-btn" data-tweetreply="${tweet.uuid}">Reply</button>
+                ${repliesHtml}
+            </div>   
+        </div>
+        `   
+   })
+   return feedHtml 
+}
+
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
+
+render()
+
+
