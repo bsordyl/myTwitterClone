8c3088a26752f25e032464bd5767c89dbebda2d6
bsordyl <sordylbartlomiej@gmail.com>
2024-01-26 10:12:17 +0100

    added ternary operators

--- index.js
+++ index.js
@@ -135,18 +135,9 @@ function getFeedHtml(){
     let feedHtml = ``
 
     tweetsData.forEach(function(tweet){
-     
-        let likeIconClass = ''
-        
-        if (tweet.isLiked){
-            likeIconClass = 'liked'
-        }
-        
-        let retweetIconClass = ''
-        
-        if (tweet.isRetweeted){
-            retweetIconClass = 'retweeted'
-        }
+
+        const likeIconClass = tweet.isLiked && 'liked' 
+        const retweetIconClass = tweet.isRetweeted && 'retweeted'
         
         let repliesHtml = ''
         
